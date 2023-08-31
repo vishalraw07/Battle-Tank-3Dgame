45a61fb1ba15b759c277e6fe7a27cb3eec286ec9
VishalRaw07 <vishalraw07.co.in@gmail.com>
2023-08-31 08:31:21 +0530

    Implemented singleton for playertank and enemytank.

---
+++ Assets/Scripts/Singleton/EnemyTank.cs
@@ -0,0 +1,10 @@
+public class EnemyTank
+{
+    // Add enemy tank-specific properties and methods here
+}
+
+
+public class EnemyTankSingleton : Singleton<EnemyTank>
+{
+    // Add additional methods or properties specific to EnemyTankSingleton here
+}
